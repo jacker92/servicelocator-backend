01798cfa39b17ee301e45c8dc24fd7a6f69495e5
jacker92 <lahtinen.jaakko@gmail.com>
2021-03-11 10:18:30 +0200

    Initial commit

---
+++ Controllers/HelsinkiServiceController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ServiceLocatorBackend.Models;
+
+namespace ServiceLocatorBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class HelsinkiServiceController : ControllerBase
+    {
+        private readonly ILogger<HelsinkiServiceController> _logger;
+
+        public HelsinkiServiceController(ILogger<HelsinkiServiceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<HelsinkiService> Get()
+        {
+            return new List<HelsinkiService> { new HelsinkiService { Name = "test" } };
+        }
+    }
+}
