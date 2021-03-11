cd687a0e7a3a8045cc6e665c7bac9f5b038a87ba
jacker92 <lahtinen.jaakko@gmail.com>
2021-03-11 13:56:18 +0200

    Removed unnecessary usings.

--- Controllers/HelsinkiServiceController.cs
+++ Controllers/HelsinkiServiceController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using ServiceLocatorBackend.Models;
 using ServiceLocatorBackend.Services;
 
 namespace ServiceLocatorBackend.Controllers
--- Services/IHelsinkiServiceService.cs
+++ Services/IHelsinkiServiceService.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using ServiceLocatorBackend.Models;
 
 namespace ServiceLocatorBackend.Services
